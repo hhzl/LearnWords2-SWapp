14c8e0a5524f0587e27112e659bea62381e4b948
hhzl <hannes.hirzel@gmail.com>
2016-12-09 22:25:50 +0100

    LW object and wds added

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2';
+const PRECACHE = 'precache-v4';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -24,7 +24,9 @@ const PRECACHE_URLS = [
   './', // Alias for index.html
   'styles.css',
   'main.css',
-  'demo.js'
+  'demo.js',
+  'LW.js',
+  'wordlist-en-ge.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
